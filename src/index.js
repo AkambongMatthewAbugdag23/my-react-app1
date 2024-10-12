c0b5505d3562a0795e2319dea48912d8177cc0ca
Matthew <matthew.abugdag23@gmail.com>
2024-10-12 19:01:46 +0000

    a

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 function FavoriteColor() {
   const [color, setColor] = useState('red');
 
-  return <h1>My favoritw color is {color}! </h1>
+  return <h1>My favoritw color is {color}!</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
