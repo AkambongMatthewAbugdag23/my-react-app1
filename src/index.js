5cd6500907cd6c63b3dc4d27ffd67b232aeb8a5d
Matthew <matthew.abugdag23@gmail.com>
2024-10-12 18:03:44 +0000

    Hi, I am a commit message.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello Matthew!</h1>;
+const myFirstElement = <h1>Hello React!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myFirstElement);
 
+/*
+You are now watching
+the React file 'index.js'
+through our 'Show React' tool.
+*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
